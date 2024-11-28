691f864a81a01a35738fead4fd999e21343b7b01
MinhKhanh614 <167750602+MinhKhanh614@users.noreply.github.com>
2024-11-28 15:49:52 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,26 @@
-// Giá trị Analog của cảm biến
-let dataAnalog = 0
-// Giá trị (%) của cảm biến
+/**
+ * Giá trị Analog của cảm biến
+ */
+/**
+ * Giá trị (%) của cảm biến
+ */
 let dataPercent = 0
-
+let dataAnalog = 0
 // Bật cổng Serial
 serial.setBaudRate(BaudRate.BaudRate115200)
-
 // Xóa toàn bộ nội dung trên LCD (nếu có)
 lcd.clearScreen()
-
 // Cho hiển thị tiêu đề trước
 lcd.displayText("Soil Moisture", 1, 1)
 lcd.displayText(lcd.displaySymbol(lcd.Symbols.sym02), 1, 2)
-
 basic.forever(function () {
     // Đọc giá trị Analog của cảm biến và đổi ra thang (%)
     dataAnalog = pins.analogReadPin(AnalogPin.P0)
     dataPercent = Math.round(Math.map(dataAnalog, 0, 1023, 100, 0))
-
     // Cho hiển thị giá trị (%) của cảm biến trên LCD
     lcd.displayText("" + dataPercent + "%  ", 3, 2)
-
     // Gửi giá trị (%) của cảm biến lên Serial
     serial.writeLine("" + (dataPercent))
-
     // Dừng 0.5s
     basic.pause(500)
 })
